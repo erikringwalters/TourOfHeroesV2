04616bd0120a7105ba7c34b6a8ed598f3e193989
Erik Ring-Walters <erik.ring.walters@gmail.com>
2018-05-22 15:57:02 -0700

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Mrs. Mean'},
+    {id: 13, name: 'Mr. Doom'},
+    {id: 14, name: 'Doom Guy'},
+    {id: 15, name: 'No Guy Buy Guy'},
+    {id: 16, name: 'Mr. Credible'},
+    {id: 17, name: 'Admiral Akbar'},
+    {id: 18, name: 'The Trap'},
+    {id: 19, name: 'Hum Darkmonth'},
+    {id: 20, name: 'The Other One'},
+];
